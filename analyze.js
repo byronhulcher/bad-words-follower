71623faa4cc7f4d3d88c503abdee955af1fd3af4
byronhulcher <byronhulcher@gmail.com>
2016-06-30 16:16:49 -0400

    Skip package names

--- analyze.js
+++ analyze.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 const matches = JSON.parse(fs.readFileSync('results.txt', 'utf-8'));
 
-var count = function(ary, classifier) {
+var count = function(ary, skipPackageNames) {
   return ary.reduce(function(counter, item) {
-    var words = (classifier || String)(item);
+    var words = item.words
     words.map(function(value, index){
         value = value.toLowerCase(value);
+        if (skipPackageNames && value === item.name){
+            return;
+        }
         counter[value] = counter.hasOwnProperty(value) ? counter[value] + 1 : 1;
     })
     
@@ -14,5 +17,8 @@ var count = function(ary, classifier) {
   }, {})
 };
 
-var countByWord = count(matches, function(match) { return match.words });
+var countByWord = count(matches);
+var countByWordWithoutPackageNames = count(matches, true);
+
 fs.writeFileSync("analysis.txt", JSON.stringify(countByWord, null, 2));
+fs.writeFileSync("analysis-without-package-names.txt", JSON.stringify(countByWordWithoutPackageNames, null, 2));
